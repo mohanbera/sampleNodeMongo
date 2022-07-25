53d42dcec1923a30a5bff02625e68e1375107962
mohanbera <immohanbera777@gmail.com>
2022-07-25 09:11:17 +0530

    removed async from getStudent

--- PostgreSQL/controllers/controllers.js
+++ PostgreSQL/controllers/controllers.js
@@ -12,7 +12,7 @@ function getStudents() {
     });
 }
 
-async function getStudent(id) {
+function getStudent(id) {
     return new Promise((resolve, reject) => {
         pool.query(queries.getStudent,[id], (error, result) => {
             if(error) reject(error);
